ffc37561a18791ff0c8270c0d8c3b2cc127427ec
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-05-27 14:19:56 +0600

    Добавить эндпоинт для уведомления при закрытии и при удалении задания

--- src/application.py
+++ src/application.py
@@ -29,11 +29,11 @@ def test():
     return ('Service is up', 200)
 
 
-@app.route('/api/v1/send-email-notification', methods=['POST'])
-def send_email_notification():
+@app.route('/api/v1/notification/new-task', methods=['POST'])
+def new_task():
     for data in request.get_json():
         message = EmailMessage()
-        message["Subject"] = "У вас новое задание"
+        message["Subject"] = "У вас новое задание!"
         message["From"] = "Task Management System"
         message["To"] = data['receiverEmail']
 
@@ -44,7 +44,84 @@ def send_email_notification():
         html = f"""\
         <html>
           <body>
-            <h3>Здравствуйте, вам было назначено новое задание: {title} от {creatorName} сроком до {dueDate}</h3>
+            <p>Здравствуйте, Вам было назначено новое задание.</p>
+            <br>
+            Наименование: {title}.
+            <br>
+            Отправитель: {creatorName}.
+            <br>
+            Cрок: {dueDate}.</p>
+          </body>
+        </html>
+        """
+        message.add_alternative(html, "html")
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+            smtp.login(sender_email, sender_password)
+            smtp.send_message(message)
+
+    return ('', 204)
+
+@app.route('/api/v1/notification/close-task', methods=['POST'])
+def close_task():
+    data = request.get_json()
+    message = EmailMessage()
+    message["Subject"] = "Задание завершено!"
+    message["From"] = "Task Management System"
+    message["To"] = data['receiverEmail']
+
+    title = data['taskTitle']
+    executorNames = data['executorNames']
+    executorPrefix = ''
+    if (len(executorNames) > 1): 
+        executorPrefix = 'Исполнители' 
+    else:
+        executorPrefix = 'Исполнитель'
+    plainExecutorNames = ', '.join(data['executorNames'])
+    createdDate = datetime.datetime.fromisoformat(data['taskCreatedDate']).strftime('%X %d %b, %Y')
+    dueDate = datetime.datetime.fromisoformat(data['taskDueDate']).strftime('%X %d %b, %Y')
+
+    html = f"""\
+    <html>
+      <body>
+        <p>Здравствуйте, созданное Вами задание было завершено.</p>
+        <br>
+        Наименование: {title}.
+        <br>
+        {executorPrefix}: {plainExecutorNames}.
+        <br>
+        Начало - {createdDate}, конец - {dueDate}.</p>
+      </body>
+    </html>
+    """
+    message.add_alternative(html, "html")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(sender_email, sender_password)
+        smtp.send_message(message)
+
+    return ('', 204)
+
+    
+@app.route('/api/v1/notification/delete-task', methods=['POST'])
+def delete_task():
+    for data in request.get_json():
+        message = EmailMessage()
+        message["Subject"] = "Задание было удалено!"
+        message["From"] = "Task Management System"
+        message["To"] = data['receiverEmail']
+
+        title = data['taskTitle']
+        creatorName = data['creatorName']
+
+        html = f"""\
+        <html>
+          <body>
+            <p>Здравствуйте, назначенное Вам задание было удалено.</p>
+            <br>
+            Наименование: {title}.
+            <br>
+            Отправитель: {creatorName}.
           </body>
         </html>
         """
